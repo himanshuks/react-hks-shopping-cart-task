7f690b270d4eb03f5fa6962d63a882351d1c31f4
Himanshu Singh <himanshu.kr5294@gmail.com>
2019-11-15 22:19:13 +0530

    redux action added

---
+++ src/actions/items.js
@@ -0,0 +1,25 @@
+import { DISPLAY_ITEMS, DELETE_ITEMS, EDIT_ITEMS } from "./types";
+
+import ItemData from "../data/data";
+
+export const getItems = () => dispatch => {
+  dispatch({
+    type: DISPLAY_ITEMS,
+    payload: ItemData
+  });
+};
+
+export const deleteItem = id => dispatch => {
+  dispatch({
+    type: DELETE_ITEMS,
+    payload: id
+  });
+};
+
+export const editItem = id => dispatch => {
+  console.log("id received is" + id);
+  dispatch({
+    type: EDIT_ITEMS,
+    payload: id
+  });
+};
---
+++ src/actions/types.js
@@ -0,0 +1,3 @@
+export const DISPLAY_ITEMS = "DISPLAY_ITEMS";
+export const DELETE_ITEMS = "DELETE_ITEMS";
+export const EDIT_ITEMS = "EDIT_ITEMS";
