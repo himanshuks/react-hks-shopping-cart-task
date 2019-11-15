c175ea25e3474002634dded486476e609605abc0
Himanshu Singh <himanshu.kr5294@gmail.com>
2019-11-15 22:18:10 +0530

    redux store implemented

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import { Provider } from "react-redux";
+import store from "./store";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import DisplayItems from "./components/DisplayItem";
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Fragment>
+          <DisplayItems />
+        </Fragment>
+      </Provider>
+    );
+  }
 }
 
 export default App;
